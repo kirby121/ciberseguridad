1619cc7792b3db92c134a813989e5827757019a3
kirby121 <83082132+kirby121@users.noreply.github.com>
2022-06-24 12:27:59 -0500

    Add files via upload

--- carousel.js
+++ carousel.js
@@ -8,16 +8,26 @@ export function carousel(leftArrow, rightArrow, title, text, arrTitle, arrText, 
 
     let content = 0
 
-    function changeContent(isLeft) {
-        (isLeft) ? content-- : content++
+    function changeContent(isLeft, circle = -1) {
 
-        $title.textContent = arrTitle[content]
-                $text.textContent = arrText[content]
-                $circles.forEach(circle => circle.classList.remove("circle-selected"));
-                $circles[content].classList.add("circle-selected")
+        (circle == -1)
+        ?  (isLeft) ? content-- : content++
+        : content = circle
+            
+            $title.textContent = arrTitle[content]
+            $text.textContent = arrText[content]
+            $circles.forEach(circle => circle.classList.remove("circle-selected"));
+            $circles[content].classList.add("circle-selected")
+    
     }
 
+    
     d.addEventListener("click", e => {
+
+        $circles.forEach((circle, index) => {
+        if(e.target === circle) changeContent(true, index)
+        });
+
         if (e.target === $leftArrow && content !== 0) changeContent(true)
 
         else if (e.target === $rightArrow && content !== arrTitle.length-1) changeContent(false)
@@ -28,5 +38,50 @@ export function carousel(leftArrow, rightArrow, title, text, arrTitle, arrText, 
         if (e.key === "ArrowLeft" && content !== 0) changeContent(true)
 
         else if (e.key === "ArrowRight" && content !== arrTitle.length-1) changeContent(false)
+    })
+}
+
+export function aboutCourseCarousel(btn, title, text, arrTitle, arrText) {
+    const d = document,
+    $buttons = d.querySelectorAll(btn),
+    $title = d.querySelector(title),
+    $text = d.querySelector(text)
+
+    $buttons.forEach((btn, index) => {
+
+        btn.addEventListener("click", e => {
+            $title.textContent = arrTitle[index]
+            $text.textContent = arrText[index]
+
+            $buttons.forEach(btn => btn.classList.remove("module-btn-selected"));
+            btn.classList.add("module-btn-selected")
+        })
+
+    });
+}
+
+export function aboutCourseCarouselCellphone(leftArrow, rightArrow, indexNum, title, text, arrTitle, arrText) {
+    const d = document,
+    $leftArrow = d.querySelector(leftArrow),
+    $rightArrow = d.querySelector(rightArrow),
+    $title = d.querySelector(title),
+    $text = d.querySelector(text),
+    $indexNum = d.querySelector(indexNum)
+
+    let content = 0
+
+    function changeContent(isLeft) {
+
+       (isLeft) ? content-- : content++
+            
+            $title.textContent = arrTitle[content]
+            $text.textContent = arrText[content]
+            $indexNum.textContent = content + 1
+    
+    }
+
+    d.addEventListener("click", e => {
+        if(e.target == $leftArrow && content !== 0) changeContent(true)
+        else if(e.target == $rightArrow && content !== (arrTitle.length - 1)) changeContent(false)
     })
 }
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import { carousel } from "./carousel.js"
+import { aboutCourseCarousel, aboutCourseCarouselCellphone, carousel } from "./carousel.js"
 import { changeImg } from "./change_image.js"
 import { hamburguerMenu } from "./hamburguer_menu.js"
 
@@ -7,5 +7,7 @@ const d = document
 d.addEventListener("DOMContentLoaded", e => {
     hamburguerMenu(".header-img", "header", ".header-top a")
     changeImg(".header-img img")
-    carousel(".left-arrow", ".right-arrow", "#about-course h2", "#about-course p", ["¿Quiénes somos?", "¿Qué queremos?", "¿Cómo lo harémos?"], ["Un pato, que va cantando alegremente cuac, cuac, cuando se encuentra a un lindo gato, miau, miau 1", "Un pato, que va cantando alegremente cuac, cuac, cuando se encuentra a un lindo gato, miau, miau 2", "Un pato, que va cantando alegremente cuac, cuac, cuando se encuentra a un lindo gato, miau, miau 3"], ".circle-inside")
+    carousel(".left-arrow", ".right-arrow", "#about-us h2", "#about-us p", ["¿Quiénes somos?", "¿Qué queremos?", "¿Cómo lo harémos?"], ["Un pato, que va cantando alegremente cuac, cuac, cuando se encuentra a un lindo gato, miau, miau 1", "Un pato, que va cantando alegremente cuac, cuac, cuando se encuentra a un lindo gato, miau, miau 2", "Un pato, que va cantando alegremente cuac, cuac, cuando se encuentra a un lindo gato, miau, miau 3"], ".circle-inside")
+    aboutCourseCarousel(".module-btn", ".course-information-text h2", ".course-information-text p", ["Módulo 1", "Módulo 2", "Módulo 3", "Módulo 4", "Módulo 5"], ["Un pato, que van cantando alegremente, cuac cuac :D 1", "Un pato, que van cantando alegremente, cuac cuac :D 2", "Un pato, que van cantando alegremente, cuac cuac :D 3", "Un pato, que van cantando alegremente, cuac cuac :D 4", "Un pato, que van cantando alegremente, cuac cuac :D 5"])
+    aboutCourseCarouselCellphone(".about-course-left-arrow", ".about-course-right-arrow", ".course-information-index", ".course-information-title", ".course-information-text p", ["Módulo 1", "Módulo 2", "Módulo 3", "Módulo 4", "Módulo 5"], ["En éste módulo, se verán los fundamentos de x cosa, además de que se presentarán las estrategias a seguir dentro del curso", "Un pato, que van cantando alegremente, cuac cuac :D 2", "Un pato, que van cantando alegremente, cuac cuac :D 3", "Un pato, que van cantando alegremente, cuac cuac :D 4", "Un pato, que van cantando alegremente, cuac cuac :D 5"])
 })
