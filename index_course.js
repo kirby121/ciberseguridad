d1a93e8bd3b8c65ffd3ca7d02d1db734f57bde0b
kirby121 <83082132+kirby121@users.noreply.github.com>
2022-08-09 08:10:44 -0500

    Update index_course.js

--- index_course.js
+++ index_course.js
@@ -2,6 +2,8 @@ import { speechVoice } from "./reading.js"
 import { addControls, pauseBtn, returnTimeBtn, startVideo, videoEnd, volumeBtn } from "./video.js"
 
 const d = document
+d.querySelector(".reading-screen button").addEventListener("click", () => window.open("https://forms.gle/CAmpVUWYdH4MxLhJA.com"))
+
 
 d.addEventListener("DOMContentLoaded", e => {
     startVideo(".video-start button", ".video-start", "#video video")
